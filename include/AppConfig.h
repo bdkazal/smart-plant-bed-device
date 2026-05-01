77ddda13f103f0667d608a7046e6d12e0dd9c31e
bdkazal <bdkazal@gmail.com>
2026-05-02 00:15:10 +0600

    Cache Laravel device config with schedules

--- include/AppConfig.h
+++ include/AppConfig.h
@@ -2,6 +2,17 @@
 
 #include <Arduino.h>
 
+static const int MAX_WATERING_SCHEDULES = 10;
+
+struct WateringScheduleConfig
+{
+  int id = 0;
+  bool isEnabled = false;
+  int dayOfWeek = 0;
+  String timeOfDay;
+  int durationSeconds = 0;
+};
+
 struct DeviceConfig
 {
   String deviceName;
@@ -11,9 +22,18 @@ struct DeviceConfig
   int maxWateringDurationSeconds = 0;
   int cooldownMinutes = 0;
   int localManualDurationSeconds = 0;
+
+  WateringScheduleConfig schedules[MAX_WATERING_SCHEDULES];
+  int scheduleCount = 0;
+
+  bool hasLoadedConfig = false;
 };
 
 extern DeviceConfig deviceConfig;
 
 void printDeviceConfig();
+
 bool parseConfigResponse(const String &response);
+bool parseConfigObjectJson(const String &configJson);
+
+String extractConfigJsonFromResponse(const String &response);
