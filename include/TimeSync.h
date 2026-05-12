58cd5d235b105878ae16e52cba1d80a06c3bdf62
bdkazal <bdkazal@gmail.com>
2026-05-12 21:09:43 +0600

    Track RTC and NTP time source

--- include/TimeSync.h
+++ include/TimeSync.h
@@ -6,7 +6,9 @@ void beginTimeSync();
 void syncTimeFromNtp(const String &timezoneName);
 
 bool isTimeReady();
+String getTimeSourceText();
 
 int getCurrentDayOfWeekIso();
 String getCurrentDateString();
 String getCurrentTimeString();
+String getCurrentHourMinuteString();
